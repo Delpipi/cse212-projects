a53d2305c3178110970acc5647c0ed9f11463df3
Delpipi <alexandrepaulkouame@gmail.com>
2025-07-07 00:08:29 +0000

    completed W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,25 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Perform "number * i" where 0 <= i < length and
+        //add the result into array of size 'length'
+
+        //create array of double number
+        var results = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            //add 1 to i and store the result
+            int factor = i + 1;
+
+            //multiply the previous result by 'number' and
+            //store the result into array at index 'i'
+            results[i] = number * factor;
+        }
+
+        //return the array of multiples of 'number'
+        return results;
     }
 
     /// <summary>
@@ -25,9 +38,18 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //GET Round in data.Count
+        amount %= data.Count;
+
+        //Get a list beginning of 'data.Count - amount' for 'amount' values
+        List<int> amountList = data.GetRange(data.Count - amount, amount);
+
+        //Insert previous list at index 0 of the data List
+        data.InsertRange(0, amountList);
+
+        //Previous operation increase the length and shift
+        //content of array by 'amount' length to the right
+        //Remove element from index 'amount' to 'data.Count - amount'
+        data.RemoveRange(data.Count - amount, amount);
     }
 }
